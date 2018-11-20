ec93664f11cc62851ff90dfc8c0aa6f7c96b24b5
Elia Neaimeh <elianeaimeh@Elias-MBP.lan>
2018-11-20 12:48:08 +0200

    add css folder, css file, app.js file

---
+++ app.js
@@ -0,0 +1,22 @@
+var submit_by_tag = function () {
+    var jsonObject= document.getElementById('jsonObject').value;
+    
+    this.IsJsonString = function(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+    if(!this.IsJsonString(jsonObject)) {
+        alert("not json");
+    } else {
+        console.log(jsonObject);
+    }
+    
+    
+}
+
+
