f78f123daba9916c7476ccc16f2492e006600a57
Elia Neaimeh <elianeaimeh@Elias-MBP.lan>
2018-11-22 23:44:56 +0200

    delete app.js file

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ const createTree = require('./js/createtree.js');
 // require('./css/styles.css');
 require('./sass/styles.scss');
 
-function submit_by_tag () {
-    var jsonObject= document.getElementById('jsonObject').value;
+function submit_by_tag() {
+    var jsonObject = document.getElementById('jsonObject').value;
     var parsed = JSON.parse(jsonObject);
     var nodeTree = createTree(parsed);
     var node = nodeTree.getRootNode();
     console.log("root-node:" + node.name);
     this.createObject = function (node) {
-            var obj={};
-            var array =[];
+            var obj = {};
+            var array = [];
             for (var n in node.supervising) {
                 array.push(this.createObject(node.supervising[n]));
             }
-            var name  = node.name;
+            var name = node.name;
             obj[name] = array;
             return obj;
     };
